9623f2b6fd9e69b1968179a186f0c769494e95fc
Andrew Belo <baconisthecure@gmail.com>
2015-09-27 20:58:58 -0400

    updates

--- OurSchoolAgenda.js
+++ OurSchoolAgenda.js
@@ -1,23 +1,74 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+  // This code only runs on the client
+  Template.body.helpers({
+    students: function () {
+      if (Session.get("studentSort") == 'alphabetical') {
+        // If hide completed is checked, filter tasks
+        return Students.find({}, {sort: {name: 1}});
+      } else {
+        // Otherwise, return all of the tasks
+        return Students.find({}, {sort: {name: -1}});
+      }
     }
   });
+    
+     
+    Template.body.events({
+        "submit .new-task": function (event) {
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+            console.log(event);
+            // Prevent default browser form submit
+          event.preventDefault();
+
+          // Get value from form element
+          var text = event.target.text.value;
+
+          // Insert a task into the collection
+          Meteor.call("addTask",text); 
+
+          // Clear form
+          event.target.text.value = "";
+              },
+        "change .hide-completed input": function (event) {
+          Session.set("hideCompleted", event.target.checked);
+        
+        }   
+    });
+    
+   Template.task.events({
+    "click .toggle-checked": function () {
+      // Set the checked property to the opposite of its current value
+      Meteor.call("setChecked", this._id, ! this.checked);
+    },
+    "click .delete": function () {
+      Meteor.call("deleteTask", this._id);
     }
   });
+      Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
+  });
+    
 }
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
-}
+
+Meteor.methods({
+  addTask: function (text) {
+    // Make sure the user is logged in before inserting a task
+    if (! Meteor.userId()) {
+      throw new Meteor.Error("not-authorized");
+    }
+ 
+    Tasks.insert({
+      text: text,
+      createdAt: new Date(),
+      owner: Meteor.userId(),
+      username: Meteor.user().username
+    });
+  },
+  deleteTask: function (taskId) {
+    Tasks.remove(taskId);
+  },
+  setChecked: function (taskId, setChecked) {
+    Tasks.update(taskId, { $set: { checked: setChecked} });
+  }
+});
